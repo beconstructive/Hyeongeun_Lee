a73a8151145791982f129b61c71450e7a6412a80
beconstructive <think.constructively@gmail.com>
2015-07-13 11:14:32 +0900

    exercise during lecture10

---
+++ Lecture10 - ggplot.R
@@ -0,0 +1,12 @@
+# load ggplot package
+library(ggplot2)
+
+# load our group dataset
+ourdata <- read.csv(file.choose())
+
+# change response's value to character
+ourdata$EMPLOYMENT = ifelse(ourdata$EMPLOYED==1, 'EMPLOYED', 'UNEMPLOYED')
+
+# draw a plot
+qplot(GPA, AGE, color=EMPLOYMENT, data=ourdata)
+
